d43379793fad3d5a3649e781a9508701f18e20cc
alecramsay <4155251+alecramsay@users.noreply.github.com>
2021-01-04 06:29:00 -0800

    Reading elections by year

--- ushouse/helpers.py
+++ ushouse/helpers.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 import sys
-import time
+import csv
+
+
+def read_elections(elections_csv):
+    """
+    Read a CSV of congressional election results with columns;
+    YEAR, STATE, XX, REP_V, DEM_V, OTH_V, TOT_V, REP_S, DEM_S, OTH_S, TOT_S, VOTE_%, SEAT_%
+    """
+
+    elections_by_year = []
+
+    try:
+        with open(elections_csv, mode="r", encoding="utf-8-sig") as f_input:
+            csv_file = csv.DictReader(f_input)
+
+            count = 0
+            for row in csv_file:
+                elections_by_year.append(row)
+                count += 1
+
+    except Exception as e:
+        print("Exception reading elections CSV")
+        sys.exit(e)
+
+    print("")
+    print(count, "election rows read.")
+    print("")
+
+    return elections_by_year 
---
+++ ushouse/main.py
@@ -0,0 +1,22 @@
+#
+# PROCESS CONGRESSIONAL ELECTIONS
+#
+
+from pathlib import Path
+
+from helpers import *
+
+
+def main():
+    cwd = Path.cwd()
+    mod_path = Path(__file__).parent
+    relative_path = '../data/results/Congressional Elections (2000 - 2020).csv'
+    filename = (mod_path / relative_path).resolve()
+    
+    elections_by_year = read_elections(filename)
+
+    pass
+
+
+# Execute the script
+main()
