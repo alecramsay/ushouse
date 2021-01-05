d5b61decac32d6b3fb20ff347e59964c68238ce2
alecramsay <4155251+alecramsay@users.noreply.github.com>
2021-01-05 07:00:35 -0800

    Added by-state pivot

--- ushouse/main.py
+++ ushouse/main.py
@@ -5,11 +5,17 @@
 from pathlib import Path
 
 from settings import *
+from states import *
 from helpers import *
 from metrics import *
 
-
 def main():
+    # Setup by-state pivot
+    by_state = {}
+    for s in states:
+        by_state[s['XX']] = { 'Name': s['State'] }
+
+    # Read in elections
     cwd = Path.cwd()
     mod_path = Path(__file__).parent
     relative_path = '../data/results/Congressional Elections (2000 - 2020).csv'
@@ -17,6 +23,7 @@ def main():
     
     elections_by_year = read_elections(filename)
 
+    # TODO - Process each election
     for item in elections_by_year:
       best = best_seats(item["REPS"], item["VOTE_%"])
       actual = item["DEM_S"]
