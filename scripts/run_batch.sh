9e053d0717dd10ff412ff8808ade58cdd51ec0cc
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-12-25 05:28:23 -0800

    Updated combined elections data for analysis

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -7,10 +7,4 @@
 # scripts/run_batch.sh
 #
 
-echo "Imputing results for 2006"
-scripts/impute_election.py 2006
-
-echo "Comparing elections for 2006"
-scripts/compare_elections.py 2006 > temp/compare-2006.txt
-
-echo "Done"
+cat Congressional\ Elections\ \(2000\ -\ 107th\).csv	Congressional\ Elections\ \(2012\ -\ 113th\).csv Congressional\ Elections\ \(2002\ -\ 108th\).csv	Congressional\ Elections\ \(2014\ -\ 114th\).csv Congressional\ Elections\ \(2004\ -\ 109th\).csv	Congressional\ Elections\ \(2016\ -\ 115th\).csv Congressional\ Elections\ \(2006\ -\ 110th\).csv	Congressional\ Elections\ \(2018\ -\ 116th\).csv Congressional\ Elections\ \(2008\ -\ 111th\).csv	Congressional\ Elections\ \(2020\ -\ 117th\).csv Congressional\ Elections\ \(2010\ -\ 112th\).csv
